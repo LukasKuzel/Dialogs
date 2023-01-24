b82e8d3d05475053297976e005497c90a29797f6
Lukas <67201471+LukasKuzel@users.noreply.github.com>
2023-01-24 21:25:49 +0100

    Learning

--- main.py
+++ main.py
@@ -1,49 +1,40 @@
+import tkinter
 from tkinter import *
+from tkinter import ttk
 
 base = Tk()
 base.geometry("500x500")
-base.title("registration form")
+base.title("App")
 
-lb1 = Label(base, text="Enter Name", width=10, font=("arial", 12))
+
+def submit():
+    try:
+        float(en1.get())
+        float(en2.get())
+        lb_answer.config(text="Submited.")
+    except ValueError:
+        lb_answer.config(text="Seed and parameter must be a number.")
+
+lb1 = Label(base, text="Enter seed:", width=10, font=("arial", 12))
 lb1.place(x=20, y=120)
-en1 = Entry(base)
+n_var = IntVar()
+n_var.set('')
+en1 = Entry(base, textvariable=n_var)
 en1.place(x=200, y=120)
+lb2 = Label(base, text="Enter parameter:", font=("arial", 12))
+lb2.place(x=20, y=160)
+n_var = IntVar()
+n_var.set('')
+en2 = Entry(base, textvariable=n_var)
+en2.place(x=200, y=160)
+lb_answer = Label(base, text="", font=("arial", 12))
+lb_answer.place(x=120, y=320)
 
-lb3 = Label(base, text="Enter Email", width=10, font=("arial", 12))
-lb3.place(x=19, y=160)
-en3 = Entry(base)
-en3.place(x=200, y=160)
 
-lb4 = Label(base, text="Contact Number", width=13, font=("arial", 12))
-lb4.place(x=19, y=200)
-en4 = Entry(base)
-en4.place(x=200, y=200)
+submit = Button(text="Submit", width=14, command=submit)
+submit.place(x=220, y=280)
 
-lb5 = Label(base, text="Select Gender", width=15, font=("arial", 12))
-lb5.place(x=5, y=240)
-vars = IntVar()
-Radiobutton(base, text="Male", padx=5, variable=vars, value=1).place(x=180, y=240)
-Radiobutton(base, text="Female", padx=10, variable=vars, value=2).place(x=240, y=240)
-Radiobutton(base, text="others", padx=15, variable=vars, value=3).place(x=310, y=240)
+quit = Button(text="Quit", width=10, command=base.destroy)
+quit.place(x=230, y=450)
 
-list_of_cntry = ("United States", "India", "Nepal", "Germany","Czech")
-cv = StringVar()
-drplist = OptionMenu(base, cv, *list_of_cntry)
-drplist.config(width=15)
-cv.set("United States")
-lb2 = Label(base, text="Select Country", width=13, font=("arial", 12))
-lb2.place(x=14, y=280)
-drplist.place(x=200, y=275)
-
-lb6 = Label(base, text="Enter Password", width=13, font=("arial", 12))
-lb6.place(x=19, y=320)
-en6 = Entry(base, show='*')
-en6.place(x=200, y=320)
-
-lb7 = Label(base, text="Re-Enter Password", width=15, font=("arial", 12))
-lb7.place(x=21, y=360)
-en7 = Entry(base, show='*')
-en7.place(x=200, y=360)
-
-Button(base, text="Register", width=10).place(x=200, y=400)
-base.mainloop()  
+base.mainloop()
